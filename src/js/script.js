a055c43b351248a89099e49e9eb9a5fa96b59e58
Nasirdin <zhaparkulov02@gmail.com>
2022-02-14 19:23:01 +0600

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,212 +1,100 @@
-// modal ----------------------------------\
-const modalClose = document.getElementById('close');
-const modal = document.getElementById('modal');
-const connectWallet = document.getElementById('connectWallet');
+// Modal -----------------------------------------
+const signBtn  = document.getElementById('signUp');
+const signOut  = document.getElementById('signOut');
+const modal    = document.getElementById('close');
+const closeBtn = document.getElementById('closeBtn');
 
-connectWallet.addEventListener('click', () => {
-    modal.style.display = 'block';
-})
+signBtn.addEventListener('click', () => {
+    modal.style.display = 'block'
+});
 
-modalClose.addEventListener('click', () => {
-    modal.style.display = 'none';
-})
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none'
+});
 
 
 
-const apiKey = 'pk_test_F7F84FB9AF538E92'
-const fm = new Fortmatic(apiKey);
-window.web3 = new Web3(fm.getProvider());
 
-//  MetaMask ----------------------------
-        
-window.userWalletAddress = null;
 
-const loginBtn = document.getElementById('loginBtn');
-const fortmaticBtn = document.getElementById('fortmaticBtn');
-const userWallet = document.getElementById('userWallet');
-const sendM = document.getElementById('sendM');
-const sendF = document.getElementById('sendF');
-const formM = document.getElementById('formM');
-const formF = document.getElementById('formF');
-const inputM = document.getElementById('inputM');
-const inputF = document.getElementById('inputF');
-const getAcc = localStorage.getItem('accountMetaMask');
-const getAccFort = localStorage.getItem('accountFortmatic');
-const walletBalance = document.getElementById('walletBalance');
-const network = document.getElementById('network');
-const formT = document.getElementById('formT');
+// ID -------------------------------------------
+const network           = document.getElementById('network'); 
+const userBalance       = document.getElementById('userBalance');
+const userAccount       = document.getElementById('userAccount');
+const btnSignFort       = document.getElementById('btnSignFort');
+const btnSignMeta       = document.getElementById('btnSignMeta');
+const metamask          = document.getElementById('metamask');
+const fortmatic         = document.getElementById('fortmatic');
+const sendMeta          = document.getElementById('sendMeta');
+const sendFort          = document.getElementById('sendFort');
+const transactionMeta   = document.getElementById('transactionMeta');
+const transactionFort   = document.getElementById('transactionFort');
+const sendTokenBtnMeta  = document.getElementById('sendTokenBtnMeta');
+const sendTokenBtnFort  = document.getElementById('sendTokenBtnFort');
+const sendTokenMeta     = document.getElementById('sendTokenMeta'); 
+const sendTokenFort     = document.getElementById('sendTokenFort'); 
 
-if(getAcc !== null) {
-    loginMetamaskBtn()
-} else if(getAcc === null) {
-    loginBtn.addEventListener('click', loginWithMetaMask)
-}
+// localStorage -------------------
+const getAccountsMeta = localStorage.getItem('accountMeta');
+const getAccountsFort = localStorage.getItem('accountFort');
 
-async function loginWithMetaMask() {
-    if(!window.ethereum) {
-        loginBtn.innerText = 'MetaMask in not installed';
-        loginBtn.style.background = "#999999";
-        loginBtn.style.color = '#fff';
-        return false
+// Login Metamask --------------------------------
+    if(getAccountsMeta !== null){
+        loginMetamask()
+    } else if(getAccountsMeta === null){
+        btnSignMeta.addEventListener('click', loginWithMetaMask)
     }
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    .catch((e) => {
-        console.error(e.message)
-        return
-    })
-    if(!accounts) { return }
-
-    window.userWalletAddress = accounts[0]
-    console.log(window.userWalletAddress);
-    localStorage.setItem('accountMetaMask', accounts)
-    loginMetamaskBtn()
-}
-
-function loginMetamaskBtn() {
-    const getAccM = localStorage.getItem('accountMetaMask');
-    window.userWalletAddress = getAccM;
-    console.log(window.userWalletAddress);
-    const mSplit = window.userWalletAddress.split('');
-    const mNewAcc = `${mSplit.slice(0, 7)}...${mSplit[mSplit.length - 1]}`
-    const acc = mNewAcc.split(',').join('');
-    userWallet.innerText = acc
-    userWallet.style.display = 'block';
-    loginBtn.innerText = 'Sign out of MetaMask';
-    fortmaticBtn.style.display = 'none';
-    connectWallet.innerText = 'Sign out';
-    connectWallet.style.marginRight = '20px';
-    formM.style.display = 'flex';
-    formT.style.display = 'flex';
-    network.style.display = 'block';
-    walletBalance.style.display = 'block';
-    loginBtn.removeEventListener('click', loginWithMetaMask)
-    setTimeout(() => {
-        loginBtn.addEventListener('click', signOutOfMetaMask)
-        modal.style.display = 'none';
-        web3.eth.getBalance(getAccM)
-        .then((e) => {
-            walletBalance.innerText = `${e / 10 ** 18} ETH`
+    
+    async function loginWithMetaMask(){
+        const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
+        .catch((e) => {
+            console.error(e.message)
+            return
         });
-        web3.eth.net.getNetworkType()
-        .then((e) => {
-            network.innerText = e
-        });
-    }, 200)
-} 
+        if(!accounts) {return};
+        localStorage.setItem('accountMeta', accounts);
+        loginMetamask()
+    }
+    
+    function loginMetamask() {
+        let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+        let accounts = localStorage.getItem('accountMeta');
+        userAccount.style.display = 'block';
+        userBalance.style.display = 'block';
+        const accountsSplit = accounts.split('');
+        const accountSlice = `${accountsSplit.slice(0, 7)}...${accountsSplit[accountsSplit.length - 1]}`;
+        userAccount.innerText = `${accountSlice.split(',').join('')}`;
+        metamask.style.display = 'block';
+        signBtn.style.display = 'none';
+        signOut.style.display = 'block';
+        network.style.display = 'block';
+        setTimeout(() => {
+            signOut.addEventListener('click', signOutOfWallet);
+            modal.style.display = 'none';
+            web3.eth.getBalance(accounts)
+            .then(e => {
+                userBalance.innerText = `${e / 10 ** 18} ETH`
+            });
+            web3.eth.net.getNetworkType().then(e => {
+                network.innerText = e;
+            });
+        }, 200);
+    }
 
-
-
-function signOutOfMetaMask() {
-    window.userWalletAddress = null;
-    userWallet.innerText = '';
-    userWallet.style.display = 'none';
-    loginBtn.innerText = 'Login with MetaMask';
-    fortmaticBtn.style.display = 'block';
-    connectWallet.innerText = 'Connect Wallet';
-    formM.style.display = 'none';
-    formT.style.display = 'none';
-    network.style.display = 'none';
-    network.innerText = '';
-    connectWallet.style.marginRight = '0'
-    loginBtn.removeEventListener('click', signOutOfMetaMask)
-    localStorage.removeItem('accountMetaMask')
-    walletBalance.style.display = 'none';
-    walletBalance.innerText = 'loading...';
-    setTimeout(() => {
-        loginBtn.addEventListener('click', loginWithMetaMask)
-    }, 100)
-}
-
-// Fortmatic -------------------------------------------------
-
-if(getAccFort !== null) {
-    loginFormaticBtn()
-} else if (getAccFort === null) {
-    fortmaticBtn.addEventListener('click', loginWithFortmatic)
-}
-
-async function loginWithFortmatic() {
-    fortmaticBtn.innerText = 'Initializing...';
-    setTimeout(() => {
-        fortmaticBtn.innerText = 'Login with Fortmatic';
-    }, 1000)
-    web3.currentProvider.enable();
-
-    web3.eth.getAccounts((error, accounts) => {
-        if(error) throw error;
-        localStorage.setItem('accountFortmatic', accounts[0]);
-        loginFormaticBtn()
-    })
-}
-
-function loginFormaticBtn () {
-    const getAccFortF = localStorage.getItem('accountFortmatic');
-    fortmaticBtn.style.display = 'block';
-    fortmaticBtn.innerText = 'Sign out of Fortmatic';
-    loginBtn.style.display = 'none';
-    connectWallet.innerText = 'Sign out';
-    connectWallet.style.marginRight = '20px';
-    const fSplit = getAccFortF.split('');
-    const fNewAcc = `${fSplit.slice(0, 7)}...${fSplit[fSplit.length - 1]}`;
-    const acc = fNewAcc.split(',').join('');
-    userWallet.innerText = acc;
-    userWallet.style.display = 'block';
-    formF.style.display = 'flex';
-    formT.style.display = 'flex';
-    network.style.display = 'block';
-    walletBalance.style.display = 'block';
-    fortmaticBtn.removeEventListener('click', loginWithFortmatic)
-    setTimeout(() => {
-        fortmaticBtn.addEventListener('click', signOutOfFortmatic)
-        modal.style.display = 'none';
-        web3.eth.getBalance(getAccFortF)
-        .then((e) => {
-            walletBalance.innerText = `${e / 10 ** 18} ETH`
-        });
-        web3.eth.net.getNetworkType()
-        .then((e) => {
-            network.innerText = e
-        });
-    }, 200)
-}
-
-function signOutOfFortmatic() {
-    window.userWalletAddress = null;
-    userWallet.innerText = ''
-    userWallet.style.display = 'none'
-    fortmaticBtn.innerText = 'Login with Fortmatic';
-    loginBtn.style.display = 'block';
-    connectWallet.innerText = 'Connect Wallet';
-    formF.style.display = 'none';
-    formT.style.display = 'none';
-    network.style.display = 'none';
-    network.innerText = '';
-    connectWallet.style.marginRight = '0';
-    fortmaticBtn.removeEventListener('click', signOutOfFortmatic)
-    localStorage.removeItem('accountFortmatic');
-    walletBalance.style.display = 'none';
-    walletBalance.innerText = 'loading...';
-    setTimeout(() => {
-        fortmaticBtn.addEventListener('click', loginWithFortmatic)
-    }, 100)
-} 
-
-
-// Send MetaMask ----------------------------
+// send Transaction ---------------------- 
 
 async function sendTransitionMetaMask(e) {
-    sendM.innerText = 'Initializing...';
+    sendMeta.innerText = 'Initializing...';
     e.preventDefault();
-    const balanceM = e.target.children[0].value;
-    const balanceValue = (balanceM * 10 ** 18).toString(16);
-    const address = e.target.children[1].value;
-    const toAddress = `${address}`;
+    const balanceMeta = e.target.children[1].value;
+    const balanceValue = (balanceMeta * 10 ** 18).toString(16);
+    const address = e.target.children[2].value;
+    const toAddress = address;
     ethereum
         .request({
             method: 'eth_sendTransaction',
             params: [
                 {
-                    from: `${getAcc}`,
+                    from: `${getAccountsMeta}`,
                     to: `${toAddress}`,
                     value: `${balanceValue}`,
                 },
@@ -214,76 +102,17 @@ async function sendTransitionMetaMask(e) {
         })
         .then((txHash) => console.log(txHash))
         .catch((error) => {
-            modalInfo.style.top = '20px'
-            modalInfo.innerText = `${error.message}`
-            setTimeout(() => {
-                modalInfo.style.top = '-100px'
-            }, 2000)
-            setTimeout(() => {
-                modalInfo.innerText = ``
-            }, 2500)
+            e.target.children[2].value = `${error.message}`
         });
         await setTimeout(() => {
-            sendM.innerText = 'Send'
+            sendMeta.innerText = 'Send'
         }, 200)
 }
 
-formM.addEventListener('submit', sendTransitionMetaMask)
+transactionMeta.addEventListener('submit', sendTransitionMetaMask);
 
-//  Send Fortmatic --------------------------------------------
-
-const modalInfo = document.getElementById('modalInfo');
-const modalInfoText = document.getElementById('moadlInfoText');
-
-async function sendTransitionFortmatic(e) {
-    sendF.innerText = 'Initializing...';
-    e.preventDefault();
-    const balanceF = e.target.children[0].value;
-    const balanceValue = (balanceF * 10 ** 18).toString(16);
-    const address = e.target.children[1].value;
-    const toAddress = `${address}`;
-    if(toAddress.length < 42) {
-        modalInfo.style.top = '20px'
-                modalInfo.innerText = 'Invalid "to" address'
-                sendF.innerText = 'Send';
-                setTimeout(() => {
-                    modalInfo.style.top = '-100px'
-                }, 2000)
-                setTimeout(() => {
-                    modalInfo.innerText = ``
-                }, 2500)
-    }
-    await web3.eth.getAccounts((error, accounts) => {
-        if (error) throw error;
-        console.log(accounts[0].length);
-        const txnParams = {
-            from: accounts[0],
-            to: `${toAddress}`,
-            value: `0x${balanceValue}`
-        }
-
-        web3.eth.sendTransaction(txnParams, (error) => {
-            if (error) {
-                modalInfo.style.top = '20px'
-                modalInfo.innerText = `${error.message}`
-                setTimeout(() => {
-                    modalInfo.style.top = '-100px'
-                }, 2000)
-                setTimeout(() => {
-                    modalInfo.innerText = ``
-                }, 2500)
-            };
-        });
-    });
-    await setTimeout(() => {
-        sendF.innerText = 'Send'
-    }, 200)
-}
-
-formF.addEventListener('click', sendTransitionFortmatic)
-
-
-const abi = [
+// ERC20 ------------------------------
+const abiErc = [
     {
       "constant": true,
       "inputs": [],
@@ -401,53 +230,147 @@ const abi = [
     }
 ]
 
+//  Send Token Metamask --------------------------------------
+sendTokenMeta.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    let amount = (e.target.children[1].value);
+    let tokenAddress = e.target.children[2].value;
+    let to = e.target.children[3].value;
+    let from = getAccountsMeta;
+    sendTokenBtnMeta.innerText = 'Initializing...';
+    sendTokenM(from, to, amount, tokenAddress);
+})
 
 
+const sendTokenM = async (from, to, amount, tokenAddress) => {
+    let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+    let contract = new web3.eth.Contract(abiErc, tokenAddress);
+    let decimals = await contract.methods.decimals(). call();
+    let unitAmount = amount * 10 **decimals;
+    sendTokenBtnMeta.innerText = 'Send';
+    return await contract.methods.transfer(to, unitAmount).send({
+        from
+    });
+}
+// Login Fortmatic ----------------------------
 
+let apiKey = 'pk_test_F7F84FB9AF538E92'
+let fm = new Fortmatic(apiKey);
+window.web3 = new Web3(fm.getProvider());
 
+if(getAccountsFort !== null){
+    loginFortmatic()
+}else if(getAccountsFort === null) {
+    btnSignFort.addEventListener('click', loginWithFortmatic)
+}
 
+async function loginWithFortmatic() {
+    btnSignFort.innerText = 'Initializing...';
+    setTimeout(() => {
+        btnSignFort.innerText = 'Fortmatic';
+    }, 2000)
+    web3.currentProvider.enable();
 
-// fortmatic -----------------------------
-formT.addEventListener('submit', async (e) => {
+    web3.eth.getAccounts((error, accounts) => {
+        if(error) throw error;
+        localStorage.setItem('accountFort', accounts[0]);
+        loginFortmatic()
+    })
+}
+
+function loginFortmatic() {
+    let accountsFort = localStorage.getItem('accountFort');
+    userAccount.style.display = 'block';
+    userBalance.style.display = 'block';
+    const accountsSplit = accountsFort.split('');
+    const accountSlice = `${accountsSplit.slice(0, 7)}...${accountsSplit[accountsSplit.length - 1]}`;
+    userAccount.innerText = `${accountSlice.split(',').join('')}`;
+    fortmatic.style.display = 'block';
+    signBtn.style.display = 'none';
+    signOut.style.display = 'block';
+    network.style.display = 'block';
+    setTimeout(() => {
+        signOut.addEventListener('click', signOutOfWallet);
+        modal.style.display = 'none';
+        web3.eth.getBalance(accountsFort)
+        .then(e => {
+            userBalance.innerText = `${e / 10 ** 18} ETH`
+        });
+        web3.eth.net.getNetworkType().then(e => {
+            network.innerText = e;
+        });
+    }, 200);
+}
+
+// send Transaction ---------------------- 
+
+async function sendTransitionFortmatic(e) {
+    sendFort.innerText = 'Initializing...';
+    e.preventDefault();
+    const balanceFort = e.target.children[1].value;
+    const balanceValue = (balanceFort * 10 ** 18).toString(16);
+    const address = e.target.children[2].value;
+    const toAddress = `${address}`;
+    if(toAddress.length < 42) {
+        e.target.children[2].value = 'Invalid "to" address';
+        sendFort.innerText = 'Send';
+    }
+    await web3.eth.getAccounts((error, accounts) => {
+        if (error) throw error;
+        const txnParams = {
+            from: accounts[0],
+            to: `${toAddress}`,
+            value: `0x${balanceValue}`
+        }
+
+        web3.eth.sendTransaction(txnParams, (error) => {
+            if (error) {
+                e.target.children[2].value = `${error.message}`;
+            };
+        });
+    });
+    await setTimeout(() => {
+        sendFort.innerText = 'Send'
+    }, 200)
+}
+    
+transactionFort.addEventListener('submit', sendTransitionFortmatic)
+    
+//  Send Token Fortmatic --------------------------------------
+
+sendTokenFort.addEventListener('submit', async (e) => {
     e.preventDefault();
     const amount = (e.target.children[1].value);
     const tokenAddress = e.target.children[2].value;
     const to = e.target.children[3].value;
-    const acc = () => {
-        if(getAcc !== null){
-            return getAcc
-        } else if(getAccFort !== null) {
-            return getAccFort
-        }
-    }
-    const from = acc()
-    
-    if(getAcc !== null){
-        const transactionParameters = {
-            nonce: '0x00',
-            to: to,
-            from: from,
-            value: `${(amount * 10 ** 18).toString(16)}`,
-            data: tokenAddress,
-            chainId: '0x3',
-          };
-          
-          const txHash = await ethereum.request({
-            method: 'eth_sendTransaction',
-            params: [transactionParameters],
-          });
-      txHash()
-    } else if(getAccFort !== null){
-        sendToken(from, to, amount, tokenAddress)
-    }
+    const from = getAccountsFort;
+    sendTokenF(from, to, amount, tokenAddress);
+    sendTokenBtnFort.innerText = 'Initializing...';
 })
 
 
-const sendToken = async (from, to, amount, tokenAddress) => {
-    const contract = new web3.eth.Contract(abi, tokenAddress);
+const sendTokenF = async (from, to, amount, tokenAddress) => {
+    const contract = new web3.eth.Contract(abiErc, tokenAddress);
     const decimals = await contract.methods.decimals(). call();
-    const unitAmount = amount * 10 **decimals
+    const unitAmount = amount * 10 **decimals;
+    sendTokenBtnFort.innerText = 'Send';
     return await contract.methods.transfer(to, unitAmount).send({
         from
     })
 }
+
+// sign Out 
+
+function signOutOfWallet() {
+        userAccount.style.display = 'none';
+        userBalance.style.display = 'none';
+        metamask.style.display = 'none';
+        fortmatic.style.display = 'none';
+        signBtn.style.display = 'block';
+        signOut.style.display = 'none';
+        userAccount.style.display = 'none';
+        network.style.display = 'none';
+        network.innerText = '......';
+        localStorage.removeItem('accountMeta');
+        localStorage.removeItem('accountFort');
+};
